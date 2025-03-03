7422742ae24ab08c1d4e61f7f15722c5663fe0ee
carrloo <carloefrem05@gmail.com>
2025-03-03 23:14:08 +0200

    Update Schedule.java

--- src/Schedule.java
+++ src/Schedule.java
@@ -51,24 +51,24 @@ public class Schedule {
         }
     }
     public static boolean isTestConflict(Date scheduledDate, int newHour, int newMinute) {
-        try (BufferedReader reader = new BufferedReader(new FileReader(SCHEDULE_FILE+"/schedule.csv"))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("schedule.csv"))) { 
             String line;
             while ((line = reader.readLine()) != null) {
                 String[] values = line.split(",");
                 if (values.length == 6 && values[2].trim().equalsIgnoreCase("Test")) {
-                    String[] timeParts = values[3].trim().split(":");
+                    String[] timeParts = values[4].trim().split(":");
                     int existingHour = Integer.parseInt(timeParts[0]);
                     int existingMinute = Integer.parseInt(timeParts[1]);
-                    String[] dateParts = values[2].split("/");
+                    String[] dateParts = values[3].split("/");
                     Date existingDate = new Date(Integer.parseInt(dateParts[0]), Integer.parseInt(dateParts[1]), Integer.parseInt(dateParts[2]));
-
+    
                     if (existingDate.equals(scheduledDate) && existingHour == newHour && existingMinute == newMinute) {
                         return true;
+                    }
                 }
             }
-    
-    }
-}
+        }
+
     catch (IOException e) {
             System.out.println("No existing test schedules found.");
         }
